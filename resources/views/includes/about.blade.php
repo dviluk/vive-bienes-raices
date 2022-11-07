726811925606b7b93743002b35a1fe14e3f57aeb
migue <miguenw@gmail.com>
2022-11-07 15:12:34 -0600

    Cambios en "quienes somos"

--- resources/views/includes/about.blade.php
+++ resources/views/includes/about.blade.php
@@ -6,23 +6,14 @@
                 <div class="agency-benefits">
                     <div class="content">
                         <div class="section-title title-2">
-                            <h2>Tenemos 10 años de experiencia</h2>
+                            <h2>Tenemos 7 años de experiencia</h2>
                         </div>
                         <p>
-                            Una de las preguntas mas frecuentes que se formulan en el sector empresarial es; ¿como proteger a mi familia, y negocio?
+                       PENDIENTE
                         </p>
                         <p>
-                            Pensando en esa incertidumbre, y la actual situación por la que atraviesa nuestro país, nace el 21 de junio de 1999, una de las empresas que hoy por hoy, ha mostrado ser una de las empresas más sólidas, disciplinadas, responsables, con alto valor por el trabajo y su capital humano. La cual se presenta hoy ante usted.
+                           PENDIENTE
                         </p>
-
-                        <p>
-                            <b>Corporación Herrera Díaz S.A. DE C.V</b>. empresa mexicana legalmente constituida, bajo la supervisión de la Fiscalía General. 
-                        </p>
-                        <p>
-                            Por lo que preocupados ante el crecimiento de la delincuencia, y el alto riesgo que implica para su familia, empresa, colaboradores, derivados de los diferentes factores que ponen en riesgo su patrimonio he inversión, provocando pérdidas, que impactan su economía y su integridad. Es por ello que nuestro equipo de trabajo, esta integrado por personal, profesional debidamente capacitado, supervisado y dotados del mejor equipo de vanguardia en los diferentes sistemas de comunicación, detección, rastreo, monitoreo .adjunto de la debida capacitación, avalada y certificada.
-                            Poniendo a sus ordenes sus expedientes para revisión y su total satisfacción
-                        </p>
-
                         <a href="{{route('contact')}}" class="btn contact-btn mt-30 btn-circle mb-100">CONTACTANOS</a>
                     </div>
                 </div>
